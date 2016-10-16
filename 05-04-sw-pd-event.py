3febfe79d8120cd5059b712c1343e445a54c4edd
Natsuki Ikeda <ikeda.natsuki@unipro.co.jp>
2016-10-16 16:39:09 +0900

    first commit

---
+++ 05-04-sw-pd-event.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+def my_callback(channel):
+    global ledState
+    if channel == 24:
+        ledState = not ledState
+        GPIO.output(25, ledState)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(24, GPIO.RISING, callback=my_callback, bouncetime=200)
+
+ledState = GPIO.LOW
+
+try:
+    while True:
+        sleep(0.01)
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
